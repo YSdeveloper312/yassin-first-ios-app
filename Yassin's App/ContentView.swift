f4ed439174b29570f893dd2d9402d9e9f4227258
Yassin Shahat <yshahat.eg@gmail.com>
2021-09-18 14:48:42 +0100

    Initial Commit

---
+++ Yassin's App/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Yassin's App
+//
+//  Created by Yassin Shahat on 07/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, please enter your name here.")
+            .padding()
+    Text("Nice to meet you Mo")
+        .padding()
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group { 
+            ContentView()
+            ContentView()
+        
+        }
+        
+
+    }
+}
---
+++ Yassin's App/Yassin_s_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yassin_s_AppApp.swift
+//  Yassin's App
+//
+//  Created by Yassin Shahat on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Yassin_s_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
